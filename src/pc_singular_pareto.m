ba27d950f884d57e8d12ee07b72cabd5b07c664e
Yang Zhao <i@snowztail.com>
2025-03-10 17:41:26 +0000

    :triangular_flag_on_post: TSP and arXiv 250310

--- src/pc_singular_pareto.m
+++ src/pc_singular_pareto.m
@@ -1,33 +1,33 @@
-% clc; clear; close; setup;
-% 
-% [transmit.antenna, reflect.antenna, receive.antenna] = deal(2, 16, 2);
-% [channel.rank, reflect.bond] = deal(min(transmit.antenna, receive.antenna), unique([1, 4, 16, reflect.antenna]));
-% assert(channel.rank == 2, 'The plot function is only for 2-sv case.');
-% [channel.pathloss.direct, channel.pathloss.forward, channel.pathloss.backward] = deal(db2pow(-65), db2pow(-54), db2pow(-46));
-% [channel.weight(1, :, 1), channel.weight(2, :, 1)] = pol2cart(pi / 4 : pi / 64 : pi * 3 / 4, 1);
-% [channel.weight(1, :, 2), channel.weight(2, :, 2)] = pol2cart(pi / 4 : -pi / 64 : -pi / 4, 1);
-% [channel.weight(1, :, 3), channel.weight(2, :, 3)] = pol2cart(5 * pi / 4 : pi / 64 : pi * 7 / 4, 1);
-% [channel.weight(1, :, 4), channel.weight(2, :, 4)] = pol2cart(5 * pi / 4 : -pi / 64 : pi * 3 / 4, 1);
-% [number.weight, number.case, number.bond, number.realization, flag.direct] = deal(size(channel.weight, 2), size(channel.weight, 3), length(reflect.bond), 2, false);
-% 
-% for r = 1 : number.realization
-% 	channel.direct = flag.direct * sqrt(channel.pathloss.direct) * fading_nlos(receive.antenna, transmit.antenna);
-% 	channel.forward = sqrt(channel.pathloss.forward) * fading_nlos(reflect.antenna, transmit.antenna);
-% 	channel.backward = sqrt(channel.pathloss.backward) * fading_nlos(receive.antenna, reflect.antenna);
-% 	channel.singular.direct(:, r) = svd(channel.direct);
-% 	for b = 1 : number.bond
-% 		for c = 1 : number.case
-% 			for w = 1 : number.weight
-% 				clear scatter_singular;
-% 				[reflect.beamformer, channel.aggregate] = scatter_singular(channel.direct, channel.forward, channel.backward, channel.weight(:, w, c), reflect.bond(b));
-% 				channel.singular.aggregate(:, w, c, b, r) = svd(channel.aggregate);
-% 			end
-% 		end
-% 	end
-% end
-% channel.singular.direct = mean(channel.singular.direct, ndims(channel.singular.direct));
-% channel.singular.aggregate = mean(channel.singular.aggregate, ndims(channel.singular.aggregate));
-% save('data/pc_singular_pareto.mat');
+clc; clear; close; setup;
+
+[transmit.antenna, reflect.antenna, receive.antenna] = deal(2, 16, 2);
+[channel.rank, reflect.bond] = deal(min(transmit.antenna, receive.antenna), unique([1, 4, 16, reflect.antenna]));
+assert(channel.rank == 2, 'The plot function is only for 2-sv case.');
+[channel.pathloss.direct, channel.pathloss.forward, channel.pathloss.backward] = deal(db2pow(-65), db2pow(-54), db2pow(-46));
+[channel.weight(1, :, 1), channel.weight(2, :, 1)] = pol2cart(pi / 4 : pi / 64 : pi * 3 / 4, 1);
+[channel.weight(1, :, 2), channel.weight(2, :, 2)] = pol2cart(pi / 4 : -pi / 64 : -pi / 4, 1);
+[channel.weight(1, :, 3), channel.weight(2, :, 3)] = pol2cart(5 * pi / 4 : pi / 64 : pi * 7 / 4, 1);
+[channel.weight(1, :, 4), channel.weight(2, :, 4)] = pol2cart(5 * pi / 4 : -pi / 64 : pi * 3 / 4, 1);
+[number.weight, number.case, number.bond, number.realization, flag.direct] = deal(size(channel.weight, 2), size(channel.weight, 3), length(reflect.bond), 2, false);
+
+for r = 1 : number.realization
+	channel.direct = flag.direct * sqrt(channel.pathloss.direct) * fading_nlos(receive.antenna, transmit.antenna);
+	channel.forward = sqrt(channel.pathloss.forward) * fading_nlos(reflect.antenna, transmit.antenna);
+	channel.backward = sqrt(channel.pathloss.backward) * fading_nlos(receive.antenna, reflect.antenna);
+	channel.singular.direct(:, r) = svd(channel.direct);
+	for b = 1 : number.bond
+		for c = 1 : number.case
+			for w = 1 : number.weight
+				clear scatter_singular;
+				[reflect.beamformer, channel.aggregate] = scatter_singular(channel.direct, channel.forward, channel.backward, channel.weight(:, w, c), reflect.bond(b));
+				channel.singular.aggregate(:, w, c, b, r) = svd(channel.aggregate);
+			end
+		end
+	end
+end
+channel.singular.direct = mean(channel.singular.direct, ndims(channel.singular.direct));
+channel.singular.aggregate = mean(channel.singular.aggregate, ndims(channel.singular.aggregate));
+save('data/pc_singular_pareto.mat');
 
 figure('Name', 'Channel Singular Value Pareto Front vs RIS Group Size', 'Position', [0, 0, 500, 400]);
 handle.color = {'#0072BD'; '#D95319'; '#EDB120'; '#7E2F8E'; '#77AC30'; '#4DBEEE'; '#A2142F'};
